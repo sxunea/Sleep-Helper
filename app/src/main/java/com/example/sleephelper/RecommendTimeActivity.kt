88bda3b53bcf9ad660d057f9afacfd1860bcea1d
chachacha12 <chaminwoo369@gmail.com>
2022-10-29 16:20:19 +0900

    Add RecommendTime xml

---
+++ app/src/main/java/com/example/sleephelper/RecommendTimeActivity.kt
@@ -0,0 +1,11 @@
+package com.example.sleephelper
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RecommendTimeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_recommend_time)
+    }
+}
