4ba6b77f50f4da5da15e06683d2929b095559692
chachacha12 <chaminwoo369@gmail.com>
2022-10-29 15:46:13 +0900

    Add WritingDiary xml

---
+++ app/src/main/java/com/example/sleephelper/WritingDiaryActivity.kt
@@ -0,0 +1,11 @@
+package com.example.sleephelper
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class WritingDiaryActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_writing_diary)
+    }
+}
